0b1f25a8e5aaa7a0cd5324e1cdbd90530cb8d172
watchYourCache <watchYourCache@gmail.com>
2023-02-17 17:01:40 +0800

    Check if .env file exists

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,10 +5,16 @@ import dotenv
 
 from .rulebot import Rulebot
 
-dotenv.load_dotenv()
-
 
 async def main() -> None:
+    if not os.path.exists(".env"):
+        raise FileNotFoundError(
+            "No .env file found. Please create one following the template in"
+            ".env.template."
+        )
+
+    dotenv.load_dotenv()
+
     bot = Rulebot()
     await bot.connect_to_db()
     await bot.start(os.environ["TOKEN"])
